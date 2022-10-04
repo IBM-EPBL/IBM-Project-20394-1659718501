9858d3095aa7506676f6f0b2962b30c27a5530fa
Shanthini1801 <113580179+Shanthini1801@users.noreply.github.com>
2022-10-04 21:33:35 +0530

    Add files via upload
    https://www.tinkercad.com/things/7q7Mkzov0Ct-sizzling-stantia/editel?sharecode=QBPY6pJsg_z7wd8TY_wShmJNLDj2u6MjoqAd1wvEFpw

---
+++ smart_home_assignment_1.ino
@@ -0,0 +1,55 @@
+int flame_sensor=5;
+int flame_detected;
+int distance;
+long duration;
+int buzzer1=9;
+int trigPin=6;
+int echoPin=7;
+int led=10;
+  
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(5,INPUT);         //flame senssor input
+  pinMode(7,INPUT);         //ultrasonic input
+  pinMode(6,OUTPUT);        //ultrasonic output
+  pinMode(10, OUTPUT);		//light ON
+  pinMode(9, OUTPUT);       //flame buzzer output
+}
+
+void ultra()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*(0.034/2);
+  if(distance>=150)
+  {
+    digitalWrite(led,HIGH);
+  }
+  else
+  {
+    digitalWrite(led,LOW);
+  }
+}
+
+void flame()
+{
+  flame_detected=digitalRead(flame_sensor);
+  if(flame_detected == 1)
+  {
+    Serial.println("Flame detected...! take action immediately.");
+    digitalWrite(buzzer1, HIGH);
+    delay(200);
+  }
+  else
+  {
+    Serial.println("No flame detected. stay cool");
+    digitalWrite(buzzer1, LOW);
+  }
+  delay(1000);
+}
+
