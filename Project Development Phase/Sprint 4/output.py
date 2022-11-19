b82095c3ac0507ec00c1addb4725b392880ec6d6
Shanthini1801 <113580179+Shanthini1801@users.noreply.github.com>
2022-11-19 22:55:40 +0530

    Update output.py

--- Project Development Phase/Sprint 4/output.py
+++ Project Development Phase/Sprint 4/output.py
@@ -1,10 +1,6 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: C:/Users/princy jincy/AppData/Local/Programs/Python/Python37/dummy.py 
 File opened
 Alert! Alert! animal detected
 Document successfully created.
 animal: detected
 moisture: 26 humidity: 59
->>> 
+Publish ok..
